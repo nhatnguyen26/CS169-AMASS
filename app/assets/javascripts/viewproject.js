36ab56b12c0ce1bde4894b342e0e16fbfd6dce8f
Phoebe Lin <phoebe.lin93@gmail.com>
2013-11-23 21:32:26 -0800

    stuck on datepicker

--- app/assets/javascripts/viewproject.js
+++
@@ -1,7 +0,0 @@
-var h1 = $('image-div').height();
-var h2 = $('info-block').height();
-
-if (h1 > h2)
-  $('info-block').height(h1);
-else
-  $('image-div').height(h2);
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,7 +6,5 @@ module ProjectsHelper
   def inplace_params(str)
     {:type => 'textarea', :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+str}
   end
-
-
 end
 
