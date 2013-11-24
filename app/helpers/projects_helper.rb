2fde01f99afdf1f7e0d4c03706080dd38739306a
Phoebe Lin <phoebe.lin93@gmail.com>
2013-11-24 13:26:23 -0800

    fixed styling and edit project

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,5 +6,10 @@ module ProjectsHelper
   def inplace_params(type,stractive)
     {:type => type, :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+stractive}
   end
+
+  def logged_in_as_project_owner(project)
+    project_owner = User.find_by_name(project.organization)
+    current_user.id == project_owner.id
+  end
 end
 
