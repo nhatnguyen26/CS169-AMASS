b90b985d8bc81fa94337dbff661dd94750eaf476
nhatnguyen26 <nathan.nguyen2604@gmail.com>
2013-11-24 03:03:02 -0800

    helper hotfix

--- app/helpers/film_makers_helper.rb
+++ app/helpers/film_makers_helper.rb
@@ -1,6 +1,6 @@
 module FilmMakersHelper
-  def inplace_params(str)
-    {:type => 'textarea', :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+str}
+  def inplace_params(type,stractive)
+    {:type => type, :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+stractive}
   end
 
   def logged_in_as_correct_user(profile_id)
