2801e9e9163aaaf31aee98e5a6523f7f6ea53c73
nhatnguyen26 <nathan.nguyen2604@gmail.com>
2013-11-24 03:26:19 -0800

    fix dating and helper method

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,8 +3,8 @@ module ProjectsHelper
   File.exist?("#{Rails.root}/app/assets/images/projects/#{id}/thumb.jpg")
   end
 
-  def inplace_params(str)
-    {:type => 'textarea', :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+str}
+  def inplace_params(type,stractive)
+    {:type => type, :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+stractive}
   end
 
 end
