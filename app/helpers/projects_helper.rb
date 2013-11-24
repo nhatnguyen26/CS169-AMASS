9bdb394251680c5bcdf8486849eb62030f5cb193
Khoa Tran <akhoa.tr@gmail.com>
2013-11-24 01:21:57 -0800

    Fixed edit button. But can't save data

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,5 +6,9 @@ module ProjectsHelper
   def inplace_params(str)
     {:type => 'textarea', :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+str}
   end
+
+  def inplace_params1(str)
+    {:type => 'date', :ok_button => 'Save', :ok_button_class => 'btn', :cancel_button => 'Cancel', :cancel_button_class => 'btn', :activator => '#'+str}
+  end
 end
 
