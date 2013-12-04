bf6dfd5b4aaeb774683bfe2d9cbdf1451112b7da
Lisa <lisa.y.watanabe@gmail.com>
2013-12-04 01:41:15 -0800

    working on how it works, edit owner check for nonprofit profiles

--- app/controllers/filmmakers_controller.rb
+++ app/controllers/filmmakers_controller.rb
@@ -1,49 +1,50 @@
 class FilmmakersController < ApplicationController
-	respond_to :html, :json
-	def index
-		@filmmaker = Filmmaker.all
+  include ApplicationHelper
+  respond_to :html, :json
+  def index
+    @filmmaker = Filmmaker.all
 
-   		filter = params[:filter]
-    		by = params[:by]
-    
-		if filter == 'all'
-			@filmmaker = Filmmaker.all
-		elsif filter == 'Specialty'
-			@filmmaker = Filmmaker.select{|x| x.specialty.to_s.include?(by)}
-		elsif filter == 'Region'
-			@filmmaker = Filmmaker.select{|x| x.location.to_s.include?(by)}
-		end
+      filter = params[:filter]
+        by = params[:by]
 
-		respond_to do |format|
-      		format.html # index.html.erb
-      		format.json { render json: @filmmaker }
-    		end
-	end
+    if filter == 'all'
+      @filmmaker = Filmmaker.all
+    elsif filter == 'Specialty'
+      @filmmaker = Filmmaker.select{|x| x.specialty.to_s.include?(by)}
+    elsif filter == 'Region'
+      @filmmaker = Filmmaker.select{|x| x.location.to_s.include?(by)}
+    end
 
-	def show
-		@filmmaker = Filmmaker.find(params[:id])
+    respond_to do |format|
+          format.html # index.html.erb
+          format.json { render json: @filmmaker }
+        end
+  end
 
-    		respond_to do |format|
-           	format.html # show.html.erb
-        	format.json { render json: @filmmaker }
-    		end
-	end
-	
-	def edit
+  def show
+    @filmmaker = Filmmaker.find(params[:id])
+
+        respond_to do |format|
+            format.html # show.html.erb
+          format.json { render json: @filmmaker }
+        end
+  end
+
+  def edit
             temp = Filmmaker.find params[:id]
             if current_user.id == temp.user.id
-         	@filmmaker = temp
-              	render
+          @filmmaker = temp
+                render
             else
-               	flash[:error] = "Illegal Command"
-              	redirect_to root_path
+                flash[:error] = "Illegal Command"
+                redirect_to root_path
             end
-	    @filmmaker = Filmmaker.find params[:id]
-	end
+      @filmmaker = Filmmaker.find params[:id]
+  end
 
-  	def update
-    	@filmmaker = Filmmaker.find params[:id]
-    	@filmmaker.update_attributes!(params[:filmmaker])
-	respond_with @filmmaker
-  	end
+    def update
+      @filmmaker = Filmmaker.find params[:id]
+      @filmmaker.update_attributes!(params[:filmmaker])
+  respond_with @filmmaker
+    end
 end
--- app/controllers/nonprofits_controller.rb
+++ app/controllers/nonprofits_controller.rb
@@ -1,17 +1,28 @@
 class NonprofitsController < ApplicationController
-	def show
-		@nonprofit = Nonprofit.find(params[:id])
-	  end
+  include ApplicationHelper
+  def show
+    @nonprofit = Nonprofit.find(params[:id])
+    end
 
-	
-	def edit
-	    @nonprofit = Nonprofit.find params[:id]
-	end
 
-  	def update
-    	@nonprofit = Nonprofit.find params[:id]
-    	@nonprofit.update_attributes!(params[:nonprofit])
-    	flash[:notice] = "Your profile was successfully updated."
-    	redirect_to nonprofit_path(@nonprofit)
-  	end
+  def edit
+    if user_signed_in?
+      if logged_in_as_correct_user(@nonprofit.id)
+        @nonprofit = Nonprofit.find params[:id]
+        render
+      else
+        flash[:error] = "You are not authorized to edit this profile"
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "You must log in first."
+    end
+  end
+
+    def update
+      @nonprofit = Nonprofit.find params[:id]
+      @nonprofit.update_attributes!(params[:nonprofit])
+      flash[:notice] = "Your profile was successfully updated."
+      redirect_to nonprofit_path(@nonprofit)
+    end
 end
