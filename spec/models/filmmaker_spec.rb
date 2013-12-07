30fea0943b6ea47c3ecaa57b1d35d304eeb1470e
Lisa <lisa.y.watanabe@gmail.com>
2013-12-07 14:41:04 -0800

    added rspec for filmmaker

--- spec/models/filmmaker_spec.rb
+++ spec/models/filmmaker_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Filmmaker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'all_specialties' do
+    it 'should return all specialties' do
+      Filmmaker.all_specialties().should == ["Crowdfunding Video", "Documentary", "Event", "Short Film" , "Nature", "Flash Animation", "3D Animation", "Stop Motion"]
+    end
+  end
+
+  describe 'all_locations' do
+    it 'should return all locations' do
+      Filmmaker.all_locations.should == ["Alameda", "San Francisco","San Mateo", "Santa Clara"]
+    end
+  end
 end
