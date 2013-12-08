c2d3724f7af4fdd3b0b5577f9e9a06978778d03b
thant hein <thant16hein@gmail.com>
2013-12-08 15:12:29 -0800

    nth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131208064143) do
+ActiveRecord::Schema.define(:version => 20131208230700) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(:version => 20131208064143) do
     t.string   "category"
     t.string   "location"
     t.integer  "nonprofit_id"
+    t.boolean  "featured"
   end
 
   create_table "users", :force => true do |t|
