f1807dd55b3ab9a5e8b21261cad19525d441d320
Khoa Tran <akhoa.tr@gmail.com>
2013-12-09 00:22:42 -0800

    Prevent non-logged-in users to access settings page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,11 @@
 class SettingsController < ApplicationController
 	def index
-		@resource = current_user.profilable
+		if user_signed_in?
+			@resource = current_user.profilable
+		else
+			flash[:error] = "You are not authorized to access this page"
+       	 	redirect_to root_path
+        return
+		end
 	end
 end
