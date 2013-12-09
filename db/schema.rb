e736cca25755bb25ee5b3c20fb75db9336e0e775
Khoa Tran <akhoa.tr@gmail.com>
2013-12-08 19:27:47 -0800

    Fixed view for nonprofit and filmmaker

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131208230700) do
+ActiveRecord::Schema.define(:version => 20131209014228) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20131208230700) do
     t.datetime "updated_at", :null => false
     t.string   "location"
     t.string   "specialty"
+    t.string   "links"
   end
 
   create_table "impressions", :force => true do |t|
