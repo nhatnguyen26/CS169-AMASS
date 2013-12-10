74987652011bf2f11282286f1d9a58652b8c1475
thant hein <thant16hein@gmail.com>
2013-12-09 20:33:46 -0800

    clean up registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,32 +4,38 @@ def create
 
     if resource.save
       yield resource if block_given?
-      if resource.active_for_authentication?
-		resource.usertype = params[:usertype]
-        set_flash_message :notice, :signed_up if is_flashing_format?
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
+      authenticated = resource.active_for_authentication?
+      check_authentication(authenticated)
+
     else
       clean_up_passwords resource
       respond_with resource
     end
   end
 
-	protected 
+  protected 
 		
-		def after_sign_up_path_for(resource)
-			if resource.filmmaker?
-				resource.profilable = Filmmaker.create!
-				resource.save!
-			elsif resource.nonprofit?
-				resource.profilable = Nonprofit.create!
-				resource.save!
-			end		
-			root_path
-		end
+  def after_sign_up_path_for(resource)
+    if resource.filmmaker?
+      resource.profilable = Filmmaker.create!
+      resource.save!
+    elsif resource.nonprofit?
+      resource.profilable = Nonprofit.create!
+      resource.save!
+    end		
+    root_path
+  end
 end
+
+  def check_authentication(authenticated)
+    if authenticated
+      resource.usertype = params[:usertype]
+      set_flash_message :notice, :signed_up if is_flashing_format?
+      sign_up(resource_name, resource)
+      respond_with resource, :location => after_sign_up_path_for(resource)
+    else
+      set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+      expire_data_after_sign_in!
+      respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+    end
+  end
